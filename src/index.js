ac320984c65f184efeb96f992cf614578aee7ae0
murtazahassan545 <murtaza.hassan@adalfi.com>
2024-04-29 22:27:58 +0500

    utils and models

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port: ${process.env.PORT}`);
+    })
+    app.on("error", () => {
+        console.log("ERRR: ", error);
+        throw error
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
